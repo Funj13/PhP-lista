79630cdd8ce0ffaf4260813eddb5c036eb6b8baa
Caique Pires <108283643+Funj13@users.noreply.github.com>
2023-02-21 14:44:15 -0300

    Create exercicio2.php

---
+++ exercicio2.php
@@ -0,0 +1,15 @@
+<?php
+
+    $nota1 = 5.0;
+    $nota2 = 7.5;
+    $nota3 = 9.0;
+    
+    $media = ($nota1 + $nota2 + $nota3) / 3;
+    
+    if ($media >= 6.0) {
+        echo "Aprovado";
+    }else{
+        echo "Reprovado";
+    }
+    
+?>
