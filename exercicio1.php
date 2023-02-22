b8cd294f5434affeeaf350213df96f29d4ca5726
Caique Pires <108283643+Funj13@users.noreply.github.com>
2023-02-22 20:36:32 -0300

    Commit two

--- exercicio1.php
+++ exercicio1.php
@@ -1,10 +1,26 @@
-<?php
-
-    $a = 2;
-    $b = 5;
-    $c = 7;
-     
-    echo "C é o maior valor dado, seu valor é $c \n";
-    echo "A é menor valor dado, seu valor é $a";
-
-?>
+<?php 
+  
+     $a = 2; 
+     $b = 5; 
+     $c = 7; 
+       
+      if ($a > $b){
+          echo "A é maior q B, seu valor é $a \n"; 
+      }elseif ($a > $c){
+          echo "A é maior q C, seu valor é $b \n"; 
+      }else{
+          echo "A é o menor valor dado, seu valor é: $a \n"
+      }
+      if ($b > $a){
+          echo "B é maior q A, seu valor é $b \n"; 
+      }elseif ($b > $c){
+          echo "B é maior q C, seu valor é $b \n"; 
+      }
+      if ($c > $a){
+          elseif($c > $b){
+              echo "C éo maior valor dado, o seu valor é: $c \n"
+          }
+      }
+      
+     
+ ?>
